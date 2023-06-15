5dfa7b5b344e89c9dea6899c007e20579f3cd40a
mradmacher <michal@radmacher.pl>
2023-06-15 07:46:36 +0000

    first steps with mongo

--- main.go
+++ main.go
@@ -3,8 +3,9 @@ package main
 import (
     "context"
     "fmt"
-    "log"
+    "errors"
     "os"
+    "encoding/json"
 
     "github.com/joho/godotenv"
     "go.mongodb.org/mongo-driver/bson"
@@ -12,20 +13,96 @@ import (
     "go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func main() {
+func connectClient() (*mongo.Client, error) {
     if err := godotenv.Load(); err != nil {
-        log.Println("No .env file found")
+        return nil, errors.New("No .env file found")
     }
 
     uri := os.Getenv("MONGODB_URI")
-    fmt.Println("%s", uri)
     if uri == "" {
-        log.Fatal("Set 'MONGODB_URI'")
+        return nil, errors.New("Set 'MONGODB_URI'")
     }
     client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
     if err != nil {
-        panic(err)
+        return nil, err
     }
+
+    return client, nil
+}
+
+type Decision struct {
+    WasteCode string `bson:"waste_code"`
+    ProcessCode string `bson:"process_code,omitempty"`
+    Quantity int `bson:"quantity,omitempty"`
+}
+
+type Address struct {
+    Line1 string
+    Line2 string
+    StateCode string
+}
+
+type Installation struct {
+    Name string
+    Nip string
+    Regon string
+    Address Address
+    Decisions []Decision
+}
+
+type InstallationRepo struct {
+    Collection *mongo.Collection
+}
+
+func (repo *InstallationRepo) purge() error {
+    _, err := repo.Collection.DeleteMany(context.TODO(), bson.D{})
+    return err
+}
+
+func (repo *InstallationRepo) add(installation *Installation) error {
+    _, err := repo.Collection.InsertOne(context.TODO(), installation)
+    if err != nil { return err }
+    return nil
+}
+
+func (repo *InstallationRepo) search(installations *[]Installation) error {
+    cursor, err := repo.Collection.Find(context.TODO(), bson.D{})
+    if err != nil { return err }
+    for cursor.Next(context.TODO()) {
+        var result Installation
+        if err := cursor.Decode(&result); err != nil {
+            return err
+        }
+        *installations = append(*installations, result)
+    }
+    if err := cursor.Err(); err != nil {
+        return err
+    }
+    return nil
+}
+
+func (repo *InstallationRepo) find(installation *Installation) error {
+    err := repo.Collection.FindOne(context.TODO(), bson.D{}).Decode(installation)
+    if err == mongo.ErrNoDocuments {
+        installation = nil
+        return nil
+    } else if err != nil {
+        return err
+    }
+    return nil
+}
+
+func loadData(filePath string, installations *[]Installation) error {
+    jsonBlob, err := os.ReadFile(filePath)
+
+    err = json.Unmarshal(jsonBlob, installations)
+    if err != nil { return err }
+    return nil
+}
+
+func main() {
+    client, err := connectClient()
+    if err != nil { panic(err) }
 
     defer func() {
         if err := client.Disconnect(context.TODO()); err != nil {
@@ -33,34 +110,28 @@ func main() {
         }
     }()
 
-    coll := client.Database("mbdo").Collection("installations")
-    //_, err = coll.InsertOne(
-    //    context.TODO(),
-    //    bson.D{{"name", "Test"}})
+    repo := InstallationRepo{Collection: client.Database("mbdo").Collection("installations")}
+    err = repo.purge()
+    if err != nil { panic(err) }
 
+    var installations []Installation
+    err = loadData("db_seed.json", &installations)
+    if err != nil { panic(err) }
 
-    var result bson.M
-    err = coll.FindOne(context.TODO(), bson.D{{"name", "Test"}}).Decode(&result)
-    if err == mongo.ErrNoDocuments {
-        fmt.Printf("No document\n")
-        return
+    for _, installation := range installations {
+        err = repo.add(&installation)
+        if err != nil { panic(err) }
     }
-    if err != nil {
-        panic(err)
-    }
+
+    var result Installation
+    err = repo.find(&result)
+    if err != nil { panic(err) }
     fmt.Println("%v", result)
 
-
-    cursor, err := coll.Find(context.TODO(), bson.D{{"name", "Test"}})
-
-    for cursor.Next(context.TODO()) {
-        if err := cursor.Decode(&result); err != nil {
-            panic(err)
-        }
-        fmt.Println("%v", result)
-
-    }
-    if err := cursor.Err(); err != nil {
-        panic(err)
+    var results []Installation
+    err = repo.search(&results)
+    if err != nil { panic(err) }
+    for _, installation := range results {
+        fmt.Println("%v", installation)
     }
 }
