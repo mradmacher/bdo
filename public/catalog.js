5819e2ede0ec0588eaa8b2c23c28aae9686f08bb
mradmacher <michal@radmacher.pl>
2023-06-15 07:46:07 +0000

    add voivodenship codes

--- public/catalog.js
+++ public/catalog.js
@@ -122,3 +122,22 @@ const codes = {
     '030101',
   ],
 }
+
+const states = [
+  "02": "Dolnośląskie",
+  "04": "Kujawsko-Pomorskie",
+  "06": "Lubelskie",
+  "08": "Lubuskie",
+  "10": "Łódzkie",
+  "12": "Małopolskie",
+  "14": "Mazowieckie",
+  "16": "Opolskie",
+  "18": "Podkarpackie",
+  "20": "Podlaskie",
+  "22": "Pomorskie",
+  "24": "Śląskie",
+  "26": "Świętokrzyskie",
+  "28": "Warmińsko-Mazurskie",
+  "30": "Wielkopolskie",
+  "32": "Zachodniopomorskie",
+]
