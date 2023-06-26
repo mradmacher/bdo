756655dc6b82a0b0eedf7d4fa19b93d00934268e
mradmacher <michal@radmacher.pl>
2023-06-26 08:37:40 +0000

    add router

--- web.go
+++ web.go
@@ -5,14 +5,26 @@ import (
   "net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+type WebApp struct{}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
   w.Header().Set("Content-Type", "text/html; charset=utf-8")
   fmt.Fprint(w, "<h1>Welcome</h1>")
 }
 
+func (app WebApp) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+  switch r.URL.Path {
+    case "/":
+      homeHandler(w, r)
+    default:
+      http.Error(w, "Page not found", http.StatusNotFound)
+  }
+}
+
+
 func main() {
-  http.HandleFunc("/", handlerFunc)
+  var webApp WebApp
 
   fmt.Println("Starting the server on :3000...")
-  http.ListenAndServe(":3000", nil)
+  http.ListenAndServe(":3000", webApp)
 }
