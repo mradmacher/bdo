0bb2649227f51a1a501c3f8ed0304b1e4419675c
mradmacher <michal@radmacher.pl>
2023-06-26 09:06:05 +0000

    use chi for routing

--- web.go
+++ web.go
@@ -3,28 +3,21 @@ package main
 import (
   "fmt"
   "net/http"
+  "github.com/go-chi/chi/v5"
 )
-
-type WebApp struct{}
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
   w.Header().Set("Content-Type", "text/html; charset=utf-8")
   fmt.Fprint(w, "<h1>Welcome</h1>")
 }
 
-func (app WebApp) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-  switch r.URL.Path {
-    case "/":
-      homeHandler(w, r)
-    default:
-      http.Error(w, "Page not found", http.StatusNotFound)
-  }
-}
-
-
 func main() {
-  var webApp WebApp
+  router := chi.NewRouter()
+  router.Get("/", homeHandler)
+  router.NotFound(func(w http.ResponseWriter, r *http.Request) {
+    http.Error(w, "Page not found", http.StatusNotFound)
+  })
 
   fmt.Println("Starting the server on :3000...")
-  http.ListenAndServe(":3000", webApp)
+  http.ListenAndServe(":3000", router)
 }
