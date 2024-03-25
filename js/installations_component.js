405e4683d41f445bc3cd954b20cdfbf6773c4d74
mradmacher <michal@radmacher.pl>
2024-03-25 11:19:21 +0000

    add installation details

--- js/installations_component.js
+++ js/installations_component.js
@@ -1,16 +1,30 @@
+function formatCode(code, dangerous) {
+  let formattedCode = [
+    code.slice(0, 2),
+    code.slice(2, 4),
+    code.slice(4, 6),
+  ].join(" ")
+  if (dangerous) {
+    formattedCode += "*"
+  } else {
+    formattedCode += " "
+  }
+  return formattedCode;
+}
 export class InstallationsComponent {
   constructor(elementId) {
-    this.element = document.getElementById(elementId)
+    this.element = document.getElementById(elementId);
   }
 
   clear() {
-    this.element.innerHTML = ''
+    this.element.innerHTML = '';
   }
 
-  addInstallation(installation) {
-    let template = document.getElementById('installation-template').content.cloneNode(true)
-    template.querySelector('.name').textContent = installation.Name
-    template.querySelector('.address').textContent = `${installation.Address.Line1},  ${installation.Address.Line2}`
+  showDetails(installation) {
+    let modal = document.querySelector('.modal.installation-details');
+    modal.querySelector('.installation-name').textContent = installation.Name
+    modal.querySelector('.installation-address').textContent = `${installation.Address.Line1},  ${installation.Address.Line2}`
+    modal.querySelector('.installation-capabilities').innerHTML = '';
     installation.Capabilities.forEach((capability, i) => {
       let capabilityTemplate = document.getElementById('installation-capability-template').content.cloneNode(true)
       let formattedCode = [
@@ -21,7 +35,6 @@ export class InstallationsComponent {
       if (capability.Dangerous) {
         formattedCode += "*"
       }
-      capabilityTemplate.querySelector('.waste-code').textContent = formattedCode
       capabilityTemplate.querySelector('.waste-code').textContent = formattedCode
       capabilityTemplate.querySelector('.process-code').textContent = capability.ProcessCode
       capabilityTemplate.querySelector('.quantity').textContent = capability.Quantity
@@ -38,8 +51,45 @@ export class InstallationsComponent {
       }
       capabilityTemplate.querySelector('.quantity').classList.add(quantityStatus)
 
-      template.querySelector('.capabilities').append(capabilityTemplate)
+      modal.querySelector('.installation-capabilities').append(capabilityTemplate)
     })
+    modal.querySelectorAll('.button.cancel').forEach((elem) => {
+      elem.addEventListener('click', (event) => {
+        modal.classList.remove('is-active');
+      })
+    })
+    modal.classList.add('is-active');
+  }
+
+  addInstallation(installation) {
+    let template = document.getElementById('installation-template').content.cloneNode(true)
+    template.querySelector('.name').textContent = installation.Name
+    template.querySelector('.address').textContent = `${installation.Address.Line1},  ${installation.Address.Line2}`
+    template.querySelector('.action.show-details').addEventListener('click', (event) => {
+      this.showDetails(installation);
+    })
+    let wasteCodes = []
+    let processCodes = []
+    installation.Capabilities.forEach((capability, i) => {
+      let formattedCode = formatCode(capability.WasteCode, capability.Dangerous);
+      if (!wasteCodes.includes(formattedCode)) {
+        wasteCodes.push(formattedCode);
+      }
+      if (!processCodes.includes(capability.ProcessCode)) {
+        processCodes.push(capability.ProcessCode);
+      }
+    });
+    wasteCodes.sort().forEach((code, i) => {
+      let wasteCodeTemplate = document.getElementById('code-template').content.cloneNode(true);
+      wasteCodeTemplate.querySelector('.code').textContent = code;
+      template.querySelector('.waste-codes').append(wasteCodeTemplate);
+    })
+    processCodes.sort().forEach((code, i) => {
+      let processCodeTemplate = document.getElementById('code-template').content.cloneNode(true)
+      processCodeTemplate.querySelector('.code').textContent = code;
+      template.querySelector('.process-codes').append(processCodeTemplate)
+    })
+
     this.element.append(template)
   }
 }
